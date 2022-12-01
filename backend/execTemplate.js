5a2731d63fe2d782e671b040c9003aa41ca91b39
Ubuntu <ubuntu@ip-172-31-10-52.eu-central-1.compute.internal>
2022-12-01 09:21:30 +0000

    small fix

--- backend/execTemplate.js
+++ backend/execTemplate.js
@@ -1,7 +1,5 @@
 const { exec } = require("child_process");
 
-// string = "printf "y\nblockwavelabs\n" | sh exec.sh"
-
 exec("bash exec.sh", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
